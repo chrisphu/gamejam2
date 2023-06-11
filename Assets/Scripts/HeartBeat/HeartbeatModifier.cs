8955e19aa898ea15ba59d613c4ca7f8e090650e5
Alejandro Mestanza <alejandro.mestanza@gmail.com>
2023-06-11 11:29:51 -0400

    Werid heartbeat mod glitch

---
+++ Assets/Scripts/HeartBeat/HeartbeatModifier.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeartbeatModifier : MonoBehaviour
+{
+    public float RampTime = 0.0f;
+    public float SpeedChangePercent = 0.0f;
+    public float VignetteChange = 0.0f;
+    public float ZoomChangePercent = 0.0f;
+
+    private HeartBeatController _heartbeat;
+
+    
+    private Collider2D _collider2d;
+
+    private void Start()
+    {
+        
+        _collider2d = transform.GetComponent<Collider2D>();
+        //_heartbeat = GameObject.FindWithTag("HeartBeat").GetComponent<HeartBeatController>();
+        _heartbeat = GameObject.FindGameObjectWithTag("HeartBeat").GetComponent<HeartBeatController>();
+    }
+
+    private void ModifyHeartbeat()
+    {
+        _heartbeat.BeatsPerMinute = 200.0f;
+    }
+
+    private void OnCollision2D(Collision2D collision2d)
+    {
+        ModifyHeartbeat();
+    }
+}
