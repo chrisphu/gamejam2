e23ca283fdf20d7093c6b006ba02cb90e0d542b3
Alejandro Mestanza <alejandro.mestanza@gmail.com>
2023-06-11 13:51:39 -0400

    Finished camera changes on modified heartbeats

--- Assets/Scripts/HeartBeat/HeartbeatModifier.cs
+++ Assets/Scripts/HeartBeat/HeartbeatModifier.cs
@@ -1,6 +1,8 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class HeartbeatModifier : MonoBehaviour
 {
@@ -10,12 +12,14 @@ public class HeartbeatModifier : MonoBehaviour
     public float ZoomChangePercent = 0.0f;
 
     private HeartBeatController _heartbeat;
-    //private CinemachineVirtualCamera _camera;
+    private CinemachineVirtualCamera _camera;
 
     private float _bpm;
     private float _durationDelay;
     private float _rampdownDelay;
     private float _newBPM;
+
+    private float _camera_zoom;
 
     private IEnumerator _resetterCoroutine;
     private IEnumerator _setterCoroutine;
@@ -28,18 +32,18 @@ public class HeartbeatModifier : MonoBehaviour
         _collider2d = transform.GetComponent<Collider2D>();
         //_heartbeat = GameObject.FindWithTag("HeartBeat").GetComponent<HeartBeatController>();
         _heartbeat = GameObject.FindGameObjectWithTag("HeartBeat").GetComponent<HeartBeatController>();
-        //_camera = GameObject.FindGameObjectWithTag("VirtualMainCamera").GetComponent<VirtualMainCamera>();
-
+        _camera = GameObject.FindGameObjectWithTag("VirtualMainCamera").GetComponent<CinemachineVirtualCamera>();
 
         _bpm = _heartbeat.BeatsPerMinute;
         SpeedChangePercent /= 100;
         ZoomChangePercent /= 100;
-
+        _camera_zoom = _camera.m_Lens.OrthographicSize;
     }
-    private IEnumerator SetHeartBeat(float bpm,float delay)
+    private IEnumerator SetHeartBeat(float bpm,float zoom,float vignette,float delay)
     {
         yield return new WaitForSeconds(delay);
-        _heartbeat.BeatsPerMinute = bpm; 
+        _camera.m_Lens.OrthographicSize = _camera_zoom / zoom;
+        _heartbeat.BeatsPerMinute = bpm;
     }
     private IEnumerator DeleteOnDelay(float delay)
     {
@@ -49,14 +53,14 @@ public class HeartbeatModifier : MonoBehaviour
     private void ModifyHeartbeat()
     {
         _newBPM = _bpm * SpeedChangePercent;
-        _setterCoroutine = SetHeartBeat(_newBPM, 0);
+        _setterCoroutine = SetHeartBeat(_newBPM, 1.15f,VignetteChange, 0);
         StartCoroutine(_setterCoroutine);
         float delay = 60 * BeatDuration / (_bpm * SpeedChangePercent);
 
         Vector3 dimensions = this.gameObject.transform.localScale;
         this.gameObject.transform.localScale = new Vector3(1.15f*dimensions.x, 1.15f*dimensions.y, 1);
 
-        StartCoroutine(SetHeartBeat(_bpm,delay));
+        StartCoroutine(SetHeartBeat(_bpm,1f,1f,delay));
         StartCoroutine(DeleteOnDelay(delay+.05f));
         
     }
