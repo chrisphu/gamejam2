38e7153356a2c2bf3aaa05ac52160fa0acb1184d
Alejandro Mestanza <alejandro.mestanza@gmail.com>
2023-06-11 23:52:37 -0400

    level selector stuff

---
+++ Assets/Scripts/MainMenu/LevelManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public void LoadLevel(int levelnum)
+    {
+        SceneManager.LoadScene(gameStartScene);
+
+    }
+}
